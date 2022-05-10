ef14aa897a894ff23f4bb067a09f371efcf727cf
Kevin <kevin.noseworthy@stjoseph.com>
2022-05-10 12:29:20 -0400

    offer_layout

--- sobeys/rewards_buy.jsx
+++ sobeys/rewards_buy.jsx
@@ -1,10 +1,10 @@
 var rewards = myRecord["rewards_points"];
-var regex = /Buy|Spend/gim
-var matches = rewards.match(regex)
+var regex = /(Buy|Spend)\s\w+[^a-z0-9]/gim;
+var matches = rewards.match(regex);
 var match;
 if(matches != null){
     
-var s = rewards.split(" ");
+var s = matches[0].split(" ");
 var m = s[1].replace(",","");
 match = m;
 
