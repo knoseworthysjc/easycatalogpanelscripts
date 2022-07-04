4d3f0c0fd430a18cf2799b625f494b195c0db599
Kevin <kevin.noseworthy@stjoseph.com>
2022-07-04 17:14:57 -0400

    rewards buy

--- sobeys/rewards_buy.lua
+++ sobeys/rewards_buy.lua
@@ -1,7 +1,14 @@
 local rec = field("rewards_points")
 local regexa = rec:match("Buy")
 local regexb = rec:match("Spend")
-if(regexa) then
+local regexone = rec:match("one")
+local regexmin = rec:match("min%w")
+if(regexone) then
+    match = "one"
+elseif(regexmin) then
+    local regMin = rec:match("%d+")
+    match = regMin
+elseif(regexa) then
 match = rec:match("Buy%s%g+"):gsub("Buy",""):gsub(",","")
 elseif (regexb) then
 match = rec:match("Spend%s%g+"):gsub("Spend","")
