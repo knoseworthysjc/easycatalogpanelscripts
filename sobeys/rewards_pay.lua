d2956baad270ec895822bebe2876bfbbccf3dcdb
Kevin <kevin.noseworthy@stjoseph.com>
2022-07-05 10:19:17 -0400

    rewards_pay_lua

--- sobeys/rewards_pay.lua
+++ sobeys/rewards_pay.lua
@@ -2,5 +2,7 @@ local rec = field("rewards_points")
 local regex = rec:match("Pay%s(.*)")
 if(regex) then
 x = split(regex," ")[1]
-return split(x,",")[1]
+y=split(x,",")[1]
+z = y:gsub("/ea","")
+return z 
 else return "" end
