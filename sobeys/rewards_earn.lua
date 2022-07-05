b7bcfa5d6ba240ab0a7d843a3a0451333c7e8f65
Kevin <kevin.noseworthy@stjoseph.com>
2022-07-05 15:51:55 -0400

    earn scene

--- sobeys/rewards_earn.lua
+++ sobeys/rewards_earn.lua
@@ -1,25 +1,20 @@
 local rec = field("rewards_points")
 local regexa = rec:match("%d+%sAir")
-local regexb = rec:match("Get%s.+%s")
-
+local regexb = rec:match("%d+%sScene")
+local regexc = rec:match("Get%s.+%s")
 if(regexa)then
-
 y = regexa
 ret = y:gsub("Air","")
 elseif(regexb) then
-
-ret=regexb
-
+y = regexb
+ret = y:gsub("Scene","")
+elseif(regexc) then
+ret = regexc
 else ret= ""
-
 end
 
 if ret == "" then 
-
 return ""
-
 else
-
-return ret:gsub("Air","")
-
+return ret
 end
