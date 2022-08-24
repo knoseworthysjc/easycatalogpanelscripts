9627cf35ba595b58bacfde8675cf661051e25a6b
Kevin <kevin.noseworthy@stjoseph.com>
2022-08-24 12:57:56 -0400

    rewards_percent_updatesd

--- sobeys/rewards_percent.lua
+++ sobeys/rewards_percent.lua
@@ -1,10 +1,16 @@
 local rec = field("rewards_points"):gsub(" %$%$ "," ")
 
 if rec == "" then return "" else
-local regexa = rec:match("%d+%%")
-local regexb = rec:match("%d+%.%d+%sOFF")
-local regexc = rec:match("%d+¢")
-local regexd = rec:match("%d+%sOFF")
+local match1 = rec:match("OFF")
+local match2 = rec:match("%%")
+
+
+if(match1 or match2) then 
+    local regexa = rec:match("%d+¢")
+    local regexb = rec:match("%d+%.%d+")
+    local regexc = rec:match("%d+%%")
+    local regexd = rec:match("%d+%sOFF")
+
 if(regexa) then
     ret=regexa
  elseif (regexb) then
@@ -12,8 +18,9 @@ if(regexa) then
 elseif (regexc) then
     ret=regexc
 elseif (regexd) then
-    ret=regexd
+    ret=regexd:gsub(" OFF","")
 else ret= "" end
+else ret="" end
 
 return ret
 end
